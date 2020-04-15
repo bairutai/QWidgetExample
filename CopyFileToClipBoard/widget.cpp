696f8391b844ab34dcfd1ba1f1f4d3c0d580ae97
bairutai <bairutai@aliyun.com>
2020-04-15 20:33:47 +0800

    1.剪切板支持多张

--- CopyFileToClipBoard/widget.cpp
+++ CopyFileToClipBoard/widget.cpp
@@ -12,6 +12,7 @@ Widget::Widget(QWidget *parent)
     connect(testBtn,&QPushButton::clicked,this,[](){
         QClipboard *board = QApplication::clipboard();
         QString testFile = "C://Users//bairutai//Desktop//test1.jpg";
+        QString testFile2 = "C://Users//bairutai//Desktop//test.png";
         //以前的写法
         QImage image(testFile);
         if (!image.isNull())
@@ -19,8 +20,11 @@ Widget::Widget(QWidget *parent)
             board->setImage(image);
         }
         //新的写法
+        QList<QString> urlList;
+        urlList<<QUrl::fromLocalFile(testFile).toString();
+        urlList<<QUrl::fromLocalFile(testFile2).toString();
         QMimeData *md = new QMimeData;
-        md->setData("text/uri-list", QUrl::fromLocalFile(testFile).toString().toLatin1());
+        md->setData("text/uri-list", urlList.join("\n").toLatin1());
         board->setMimeData(md);
     });
 }
